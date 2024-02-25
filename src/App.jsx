e3cb8107287a9f6958b3cb760a98bb022a3d2c85
hrithiktamta05 <121002928+hrithiktamta05@users.noreply.github.com>
2024-02-25 21:02:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './Component/Header';
 import Content from './Component/Content';
-import './style.css';
+import './index.css';
 
 function App() {
   return (
