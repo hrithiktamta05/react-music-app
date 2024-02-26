2541cb2ca7eebb3f447a2716d32113b067ad2072
hrithiktamta05 <121002928+hrithiktamta05@users.noreply.github.com>
2024-02-26 23:20:47 +0530

    added sections in content

---
+++ src/Component/Lside.jsx
@@ -0,0 +1,11 @@
+function Lside(){
+    return (
+        <div className="l-side">
+            <ul>
+                <li>Home</li>
+                <li>Search</li>
+            </ul>
+        </div>
+    );
+}
+export default Lside;
---
+++ src/Component/Rside.jsx
@@ -0,0 +1,6 @@
+function Rside(){
+    return (
+        <div className="r-side"></div>
+    );
+}
+export default Rside;
