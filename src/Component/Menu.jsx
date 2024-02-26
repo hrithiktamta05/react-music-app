ee32ab39ca5f32821dcdec4400de665bea75ac52
Gunvantrajk <gunvantrajk@gmail.com>
2024-02-26 21:20:37 +0530

    first commit changing style

--- src/Component/Menu.jsx
+++ src/Component/Menu.jsx
@@ -2,11 +2,11 @@ function Menu(){
     return (
         <div className="menu">
             <ul>
-                <li>a</li>
-                <li>b</li>
-                <li>c</li>
-                <li>d</li>
-                <li>e</li>
+                <li>Home</li>
+                <li>My Playlist</li>
+                <li>Favorites</li>
+                <li>Search</li>
+                <li id="premium">Premium</li>
             </ul>
         </div>
     );
